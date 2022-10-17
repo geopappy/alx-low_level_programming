1361105d54b9f4a656a9715b7dd77aee169066d8
geopappy <olatunjiabayomi.o@gmail.com>
2022-10-17 08:58:02 -0700

    Add C file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+		putchar(character);
+	for (character = 'A'; character <= 'Z'; character++)
+		putchar(character);
+	putchar('\n');
+
+	return (0);
+}
