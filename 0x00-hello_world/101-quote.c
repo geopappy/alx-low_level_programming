7e885dbcaa65e0ebc3359fcdae7ce148e0231036
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-08 23:41:08 +0100

    refractor the code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-
 /**
  * main - Entry point
  *
- * Return: 1 (Error)
+ * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
-	const char *errorMsg = "and that piece of art is
-		useful\" - Dora Korpar, 2015-10-19";
-
-	write(2, errorMsg, strlen(errorMsg));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
-
 }
