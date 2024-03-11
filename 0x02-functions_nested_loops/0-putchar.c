d3f2e62cdee1e2e4f581994f5f75804cf5756d52
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-11 01:11:17 +0100

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *s = "_putchar";
+	int i = 0
+		;
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
